3d0bc0e8edaa9db9c3b971c5395974b92660c031
NikolayNikiforovBEC192 <72613544+NikolayNikiforovBEC192@users.noreply.github.com>
2024-05-02 21:21:29 +0300

    Add files via upload

--- testing_hw/task-testing-python/tests/test_02/test_size_tree.py
+++ testing_hw/task-testing-python/tests/test_02/test_size_tree.py
@@ -1,1 +1,30 @@
+import tempfile
+import os
+from tree_utils_02.size_tree import SizeTree
+
+with tempfile.TemporaryDirectory(dir=os.getcwd()) as a:
+    with tempfile.TemporaryDirectory(dir=a) as b:
+        with tempfile.TemporaryDirectory(dir=b) as c:
+            size = SizeTree().construct_filenode(a, is_dir=False).size
+            assert 4096 == size
+
+            size = SizeTree().construct_filenode(a, is_dir=True).size
+            assert 4096 == size
+
+            size = SizeTree().update_filenode(file_node=SizeTree().construct_filenode(a, is_dir=True)).size
+            assert 4096 == size
+
+            size = SizeTree().update_filenode(file_node=SizeTree().construct_filenode(a, is_dir=False)).size
+            assert 4096 == size
+
+            new_node = SizeTree().construct_filenode(a, is_dir=True)
+            new_node.children.append(SizeTree().construct_filenode(a, is_dir=True))
+
+            size = SizeTree().update_filenode(new_node).size
+            assert 4096*2 == size
+
+
+
+
+
 
--- testing_hw/task-testing-python/tests/test_02/test_tree.py
+++ testing_hw/task-testing-python/tests/test_02/test_tree.py
@@ -1,1 +1,50 @@
+import tempfile
+import os
+from tree_utils_02.tree import Tree
+import pytest
+
+
+with tempfile.TemporaryDirectory(dir=os.getcwd()) as a:
+    with tempfile.TemporaryDirectory(dir=a) as b:
+        with tempfile.TemporaryDirectory(dir=b) as c:
+            treee_node = Tree().get(a, True)
+            assert True == treee_node.is_dir
+
+            treee_node = Tree().get(a, dirs_only = False)
+            assert True == treee_node.is_dir
+
+            with pytest.raises(AttributeError) as e:
+                 Tree().get('eshkereeeeee', dirs_only = True, recurse_call=True)
+            assert 'Path not exist' in str(e)
+
+            file = open(os.path.join(a, '2.txt'), 'w')
+            assert False == Tree().get(os.path.join(a, '2.txt'), dirs_only=False).is_dir
+
+            with pytest.raises(AttributeError) as e:
+                 Tree().get(os.path.join(a, '2.txt'), dirs_only=True)
+            assert 'Path is not directory' in str(e)
+
+            assert not Tree().get(os.path.join(a, '2.txt'), dirs_only=True, recurse_call=True)
+
+
+            assert True == Tree().construct_filenode(a, True).is_dir
+
+            node = Tree().construct_filenode(a, True)
+            assert True == Tree().update_filenode(node).is_dir
+
+
+            treee_node = Tree().get(a, True)
+            assert not Tree().filter_empty_nodes(treee_node, a)
+
+            treee_node = Tree().get(a, True)
+            treee_node.is_dir = False
+            assert not Tree().filter_empty_nodes(treee_node, a)
+
+
+with tempfile.TemporaryDirectory(dir=os.getcwd()) as a:
+    with pytest.raises(ValueError) as e:
+         treee_node = Tree().get(a, True)
+         Tree().filter_empty_nodes(treee_node)
+    assert 'Code should not be executed here!' in str(e)
+            
 
